0407f2cd7208fe4800809f3441610ba8cf4d70d5
AyeshaMuduli <ece.24beca94@silicon.ac.in>
2026-05-28 16:27:12 +0530

    celsius to fafrenheit

---
+++ Question16a.kt
@@ -0,0 +1,8 @@
+// 16. Write a kotlin program to convert temperature : - celcius to fahrenheit
+fun main() {
+    val celsius = 100.0
+
+    val fahrenheit = (celsius * 9 / 5) + 32
+
+    println("$celsius°C = $fahrenheit°F")
+}
