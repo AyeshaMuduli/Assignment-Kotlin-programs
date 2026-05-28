557523d3a004e8b3bf304f1e5fa19f8cd1a6ce2f
AyeshaMuduli <ece.24beca94@silicon.ac.in>
2026-05-28 14:37:16 +0530

    Create Main.kt

---
+++ Question12b/Main.kt
@@ -0,0 +1,12 @@
+// 12. write a program to swap two numbers   - without using third variable.
+fun main() {
+    var a=10
+    var b=30
+    
+    a=a+b   
+    b=a-b   
+    a=a-b   
+    
+    println("a= $a")
+    println("b= $b")
+}
