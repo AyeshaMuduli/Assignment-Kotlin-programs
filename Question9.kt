29d410bfd58fe1cf6167c30cc01646764ab953dc
AyeshaMuduli <ece.24beca94@silicon.ac.in>
2026-05-28 15:49:07 +0530

    multiplication table

---
+++ Question9.kt
@@ -0,0 +1,7 @@
+// 9. Write a kotlin program to print the multiplication table of a given number.
+fun main(){
+    var num=5
+    for(i in 1..10){
+        println("$num X $i = ${num*i}")
+    }
+}
